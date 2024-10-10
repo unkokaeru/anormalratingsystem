95c653052223a4ebf971f87e3db5708f357fd6cf
unkokaeru <william@fayers.com>
2024-10-10 16:26:15 +0100

    docs: fix doc strings

--- source/anormalratingsystem/__init__.py
+++ source/anormalratingsystem/__init__.py
@@ -1,3 +1,3 @@
-"""Rate anything from a scale of 1-10 with simple comparisons! The end result is even normally distributed. Do you like A better than B?"""
+"""Rate anything from a scale of 1-10 with simple comparisons."""
 
 __version__ = "0.1.0"
