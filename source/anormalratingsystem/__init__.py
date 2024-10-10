4ca21a7e86135574c2cff4517b924b1d8927ccb1
unkokaeru <william@fayers.com>
2024-10-10 16:20:33 +0100

    chore: bump version from 0.0.0 to 0.1.0

--- source/anormalratingsystem/__init__.py
+++ source/anormalratingsystem/__init__.py
@@ -1,3 +1,3 @@
 """Rate anything from a scale of 1-10 with simple comparisons! The end result is even normally distributed. Do you like A better than B?"""
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
