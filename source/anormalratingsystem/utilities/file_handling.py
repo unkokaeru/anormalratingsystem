757b7e956d2fd508304ca7e499e1d9072b9aaa5b
unkokaeru <william@fayers.com>
2024-10-16 16:16:06 +0100

    fix: read/write correct format

--- source/anormalratingsystem/utilities/file_handling.py
+++ source/anormalratingsystem/utilities/file_handling.py
@@ -1,6 +1,7 @@
 """file_handling.py: File handling utilities for the application."""
 
 import csv
+import json
 from pathlib import Path
 
 from . import logger
@@ -35,8 +36,14 @@ def read_csv(file_path: Path) -> list[dict[str, str]]:
 
     with open(file_path) as file:
         csv_reader = csv.DictReader(file)
-        logger.debug(f"CSV file read successfully from {file_path} as {csv_reader}")
-        return list(csv_reader)
+        entries = []
+        for row in csv_reader:
+            if row["name"] == "Completed":
+                # Parse the JSON-like string into a list of dictionaries
+                media_entries = json.loads(row["entries"].replace('""', '"'))
+                entries.extend(media_entries)
+        logger.debug(f"CSV file read successfully from {file_path} with entries: {entries}")
+        return entries
 
 
 def write_csv(file_path: Path, data: list[dict[str, str]]) -> None:
@@ -61,9 +68,16 @@ def write_csv(file_path: Path, data: list[dict[str, str]]) -> None:
         logger.error(f"Output file {file_path} is not a CSV file.")
         raise ValueError("Output file must be a CSV file.")
 
-    with open(file_path, "w") as file:
-        csv_writer = csv.DictWriter(file, fieldnames=data[0].keys())
+    with open(file_path, "w", newline="") as file:
+        csv_writer = csv.DictWriter(file, fieldnames=["entries", "name"])
         csv_writer.writeheader()
-        csv_writer.writerows(data)
+
+        # Prepare the data for writing
+        watching_data = {
+            "entries": json.dumps(data),  # Convert list of dictionaries back to JSON-like string
+            "name": "Completed",
+        }
+
+        csv_writer.writerow(watching_data)
 
     logger.debug(f"CSV file written successfully to {file_path}")
